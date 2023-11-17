41401d1d448c7b88a41e11faf3e8cab95f596f38
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2023-11-17 18:20:13 +0100

    Added individual stations routes

--- server.ts
+++ server.ts
@@ -5,35 +5,49 @@ const port = process.env.PORT || 3003;
 
 const server = http.createServer((req, res) => {
   const { url, method } = req;
-  console.log(url);
-  if (method === "GET") {
-    if (url === "/trains") {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "application/json");
-      fs.readFile("./trains.json", "utf-8", (err, data) => {
-        if (err) {
-          return res.end(err);
-        }
-        return res.end(data);
-      });
-    } else if (url === "/stations") {
-      res.statusCode = 200;
-    //  res.setHeader("Content-Type", "application/json");
-      fs.readFile("./stations.json", "utf-8", (err, data) => {
-        if (err) {
-          return res.end(err);
-        }
-        return res.end(data);
-      });
-    } else {
-      res.statusCode = 404;
-      res.setHeader("Content-Type", "text/plain");
-      res.end("404 Not Found");
-    }
-  } else {
+  if (method !== "GET") {
     res.statusCode = 405;
     res.setHeader("Content-Type", "text/plain");
     res.end("405 Method Not Allowed");
+    return;
+  }
+  if (url === "/trains") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    fs.readFile("./trains.json", "utf-8", (err, data) => {
+      if (err) {
+        return res.end(err);
+      }
+      return res.end(data);
+    });
+  } else if (url === "/stations") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    fs.readFile("./stations.json", "utf-8", (err, data) => {
+      if (err) {
+        return res.end(err);
+      }
+      return res.end(data);
+    });
+  } else if (url && url.startsWith("/stations/")){
+    const station = url.split('/')[2];
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    fs.readFile("./stations.json", "utf-8", (err, data) => {
+      if (err) {
+        return res.end(err);
+      }
+      const json = JSON.parse(data);
+      return res.end(JSON.stringify(json.stations[station]));
+    });
+  } else if (url === "/"){
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Welcome to the API!")
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("404 Not Found");
   }
 });
 
