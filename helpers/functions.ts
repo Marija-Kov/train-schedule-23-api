ab01b3794aca154e3e21ea8c814c3b8913d3be80
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2024-04-08 22:12:36 +0200

    Specified more types, formatted - Refactored functions.ts > writeStationsEndpoint to take hardcoded value of holidays since it's only used in writeStationsEndpoint in its unmodified form. It's not used in any calculations so there might not be a good reason to define a type for it.

--- helpers/functions.ts
+++ helpers/functions.ts
@@ -1,32 +1,42 @@
-import fs from 'fs';
-import { Train, Station, StationDeparture } from "../trainScheduleTypes";
+import fs from "fs";
+import {
+  TrainIdDirection1,
+  TrainIdDirection2,
+  Train,
+  StationName,
+  StationNameFormatted,
+  Station,
+  StationDeparture,
+} from "../trainScheduleTypes";
 
-export function extractDepartureTimes(dataStr: string): string[][]{
-      /*
+export function extractDepartureTimes(dataStr: string): string[][] {
+  /*
        Takes in data string extracted from the PDF 
        and returns all times of departures of any train in an array
        for each station in the given direction.
       */
-    const len = dataStr.length + 1;
-    let index = 0; // there's always a char at position 0
-    const arr: string[] = [];
-    for(let i = 1; i < len - 1; ++i){
-      const prev = dataStr.charAt(i-1);
-      const curr = dataStr.charAt(i);
-      const next = dataStr.charAt(i+1);
-      if (curr === " " ){
-       if (prev.match(/[0-9]/) && !next.match(/[0-9]/) || 
-           !prev.match(/[0-9]/) && next.match(/[0-9]/)){
-             arr.push(dataStr.slice(index, i));
-             index = i + 1; 
-           }
-         }
-      if (!next){
-        arr.push(dataStr.slice(index, i+1));
+  const len = dataStr.length + 1;
+  let index = 0; // there's always a char at position 0
+  const arr: string[] = [];
+  for (let i = 1; i < len - 1; ++i) {
+    const prev = dataStr.charAt(i - 1);
+    const curr = dataStr.charAt(i);
+    const next = dataStr.charAt(i + 1);
+    if (curr === " ") {
+      if (
+        (prev.match(/[0-9]/) && !next.match(/[0-9]/)) ||
+        (!prev.match(/[0-9]/) && next.match(/[0-9]/))
+      ) {
+        arr.push(dataStr.slice(index, i));
+        index = i + 1;
       }
     }
-    return arr.filter(a => arr.indexOf(a) % 2 !== 0).map(a => a.split(" "));
+    if (!next) {
+      arr.push(dataStr.slice(index, i + 1));
+    }
   }
+  return arr.filter((a) => arr.indexOf(a) % 2 !== 0).map((a) => a.split(" "));
+}
 
 /*
  Timetable matrix creators modify departureTimes which is 
@@ -35,203 +45,212 @@ export function extractDepartureTimes(dataStr: string): string[][]{
 */
 
 export function createTimetableMatrix_d1(
-    departureTimes: string[][],
-    stationsArr: string[],
-    trainIdArr: number[]
-    ): string[][] {
-    let j = 0;
-    let i = 1; 
-    while (j < trainIdArr.length){
-     if(Number(departureTimes[i][j]) < Number(departureTimes[i-1][j])){
-        while(--i+1){
-          departureTimes[i].splice(j, 0, "n/a");
-          }
-       } 
-        ++i;
-       if(i === 15 || i === 0 ) {
-         i = 1;
-         ++j
-        }
-    }
-    j = 0;
-    while(j < trainIdArr.length){
-      if(departureTimes[0][j] === "n/a"){
-        i = 8; // stations.indexOf the destination of shorter routes (Beograd centar)
-        while(i < stationsArr.length){
-          departureTimes[i].splice(j, 0, "n/a")
-          ++i
-        }
+  departureTimes: string[][],
+  stationsArr: StationName[],
+  trainIdArr: TrainIdDirection1[]
+): string[][] {
+  let j = 0;
+  let i = 1;
+  while (j < trainIdArr.length) {
+    if (Number(departureTimes[i][j]) < Number(departureTimes[i - 1][j])) {
+      while (--i + 1) {
+        departureTimes[i].splice(j, 0, "n/a");
       }
-      ++j
     }
-    return departureTimes
+    ++i;
+    if (i === 15 || i === 0) {
+      i = 1;
+      ++j;
+    }
   }
+  j = 0;
+  while (j < trainIdArr.length) {
+    if (departureTimes[0][j] === "n/a") {
+      i = 8; // stations.indexOf the destination of shorter routes (Beograd centar)
+      while (i < stationsArr.length) {
+        departureTimes[i].splice(j, 0, "n/a");
+        ++i;
+      }
+    }
+    ++j;
+  }
+  return departureTimes;
+}
 
 export function createTimetableMatrix_d2(
-    departureTimes: string[][],
-    stationsArr: string[],
-    trainIdArr: number[]
-    ): string[][] {
-    let j = 0;
-    let i = 1; 
-    while (j < trainIdArr.length){
-     if(departureTimes[i] && Number(departureTimes[i][j]) < Number(departureTimes[i-1][j])){
-        while(--i+1){
-          departureTimes[i].splice(j, 0, "n/a");
-          }
-       } 
-        ++i;
-       if(i === 15 || i === 0 ) {
-         i = 1;
-         ++j
-        }
+  departureTimes: string[][],
+  stationsArr: StationName[],
+  trainIdArr: TrainIdDirection2[]
+): string[][] {
+  let j = 0;
+  let i = 1;
+  while (j < trainIdArr.length) {
+    if (
+      departureTimes[i] &&
+      Number(departureTimes[i][j]) < Number(departureTimes[i - 1][j])
+    ) {
+      while (--i + 1) {
+        departureTimes[i].splice(j, 0, "n/a");
+      }
     }
-    j = 0;
-    while(j < trainIdArr.length){
-      if(j===10){
-        i = 8; // stations.indexOf the stations right after the destination of second shortest routes (Novi Beograd)
-        while(i < stationsArr.length){
-          departureTimes[i].splice(j, 0, "n/a")
-          ++i
-        }
-      }
-      if(departureTimes[0][j] === "n/a" || j===13 || j===26 || j===35){
-        i = 11; // stations.indexOf the stations right after the destination of shortest routes (Altina)
-        while(i < stationsArr.length){
-          departureTimes[i].splice(j, 0, "n/a")
-          ++i
-        }
-      }
-      ++j
-    } 
-    return departureTimes
+    ++i;
+    if (i === 15 || i === 0) {
+      i = 1;
+      ++j;
+    }
   }
+  j = 0;
+  while (j < trainIdArr.length) {
+    if (j === 10) {
+      i = 8; // stations.indexOf the stations right after the destination of second shortest routes (Novi Beograd)
+      while (i < stationsArr.length) {
+        departureTimes[i].splice(j, 0, "n/a");
+        ++i;
+      }
+    }
+    if (departureTimes[0][j] === "n/a" || j === 13 || j === 26 || j === 35) {
+      i = 11; // stations.indexOf the stations right after the destination of shortest routes (Altina)
+      while (i < stationsArr.length) {
+        departureTimes[i].splice(j, 0, "n/a");
+        ++i;
+      }
+    }
+    ++j;
+  }
+  return departureTimes;
+}
 
 export function shapeTrainsData(
-    trainIdsDir1: number[], 
-    trainIdsDir2: number[], 
-    weekendsAndHolidays1: (boolean | "w&h_only")[], 
-    weekendsAndHolidays2: (boolean | "w&h_only")[], 
-    stations: string[], 
-    timetable1: string[][], 
-    timetable2: string[][], 
-    ): Train[] {
-   const len1 = trainIdsDir1.length;
-   const len2 = trainIdsDir2.length;
-   const arr: Train[] = [];
-   for (let i = 0; i < len1; ++i){
+  trainIdsDir1: TrainIdDirection1[],
+  trainIdsDir2: TrainIdDirection2[],
+  weekendsAndHolidays1: (boolean | "w&h_only")[],
+  weekendsAndHolidays2: (boolean | "w&h_only")[],
+  stations: StationName[],
+  timetable1: string[][],
+  timetable2: string[][]
+): Train[] {
+  const len1 = trainIdsDir1.length;
+  const len2 = trainIdsDir2.length;
+  const arr: Train[] = [];
+  for (let i = 0; i < len1; ++i) {
     const obj: Train = {
-        id: 0,
-        directionId: 1,
-        activeOnWeekendsAndHolidays: false,
-        itinerary: []
+      id: 8003,
+      directionId: 1,
+      activeOnWeekendsAndHolidays: false,
+      itinerary: [],
     };
     obj.id = trainIdsDir1[i];
     obj.directionId = 1;
     obj.activeOnWeekendsAndHolidays = weekendsAndHolidays1[i];
     obj.itinerary = [];
-    for (let j = 0; j < stations.length; ++j){
-      if(timetable1[j][i] !== "n/a"){
-      const objD: {station: string, time: number} = {
-          station: '',
-          time: 0
-      };
-      objD.station = stations[j];
-      objD.time = Number(timetable1[j][i]);
-      obj.itinerary.push(objD)
+    for (let j = 0; j < stations.length; ++j) {
+      if (timetable1[j][i] !== "n/a") {
+        const objD: { station: StationName; time: number } = {
+          station: "batajnica",
+          time: 0,
+        };
+        objD.station = stations[j];
+        objD.time = Number(timetable1[j][i]);
+        obj.itinerary.push(objD);
       }
     }
-    arr.push(obj)
+    arr.push(obj);
   }
-  for (let i = 0; i < len2; ++i){
+  for (let i = 0; i < len2; ++i) {
     const obj: Train = {
-        id: 0,
-        directionId: 1,
-        activeOnWeekendsAndHolidays: false,
-        itinerary: []
+      id: 8003,
+      directionId: 1,
+      activeOnWeekendsAndHolidays: false,
+      itinerary: [],
     };
     obj.id = trainIdsDir2[i];
     obj.directionId = 2;
     obj.activeOnWeekendsAndHolidays = weekendsAndHolidays2[i];
     obj.itinerary = [];
     const stLen = stations.length;
-    for (let j = 0; j < stLen; ++j){
-      if(timetable2[j][i] !== "n/a"){
-      const objD: {station: string, time: number} = {
-          station: '',
-          time: 0
-      };
-      objD.station = stations[stLen - 1 - j];
-      objD.time = Number(timetable2[j][i]);
-      obj.itinerary.push(objD)
+    for (let j = 0; j < stLen; ++j) {
+      if (timetable2[j][i] !== "n/a") {
+        const objD: { station: StationName; time: number } = {
+          station: "batajnica",
+          time: 0,
+        };
+        objD.station = stations[stLen - 1 - j];
+        objD.time = Number(timetable2[j][i]);
+        obj.itinerary.push(objD);
       }
     }
-    arr.push(obj)
-   }
-   return arr
+    arr.push(obj);
   }
+  return arr;
+}
 
-export function writeTrainsEndpoint(arr: Train[]){
+export function writeTrainsEndpoint(arr: Train[]) {
   const trains: any = {};
-  for (let train of arr){
-   trains[train.id] = train;
+  for (let train of arr) {
+    trains[train.id] = train;
   }
-  return fs.writeFile("../trains.json", JSON.stringify(trains, null, 2), (err) => {
-   console.log(err)
-  })
-  }
+  return fs.writeFile(
+    "../trains.json",
+    JSON.stringify(trains, null, 2),
+    (err) => {
+      console.log(err);
+    }
+  );
+}
 
 export function shapeStationsData(
-    stations: string[], 
-    stationsFormatted: string[], 
-    trainsData: Train[],
-   ): Station[] {
-    const arr: Station[] = [];
-    const stLen = stations.length;
-    for(let i = 0; i < stLen; ++i){
-      const obj: Station = {
-          name: '',
-          nameFormatted: '',
-          departures: []
-      };
-      obj.name = stations[i];
-      obj.nameFormatted = stationsFormatted[i];
-      obj.departures = [];
-      const trLen = trainsData.length;
-      for (let j = 0; j < trLen; ++j){
-        if(trainsData[j].itinerary[i]){
-          const departure: StationDeparture = {
-            time: 0,
-            trainDetails: {
-                id: 0,
-                directionId: 1,
-                activeOnWeekendsAndHolidays: false,
-            }
-          };
-          departure.time = Number(trainsData[j].itinerary[i].time);
-          departure.trainDetails = {
-            id: trainsData[j].id,
-            directionId: trainsData[j].directionId,
-            activeOnWeekendsAndHolidays: trainsData[j].activeOnWeekendsAndHolidays,
-          }
-          obj.departures.push(departure);
-         }
+  stations: StationName[],
+  stationsFormatted: StationNameFormatted[],
+  trainsData: Train[]
+): Station[] {
+  const arr: Station[] = [];
+  const stLen = stations.length;
+  for (let i = 0; i < stLen; ++i) {
+    const obj: Station = {
+      name: "batajnica",
+      nameFormatted: "Batajnica",
+      departures: [],
+    };
+    obj.name = stations[i];
+    obj.nameFormatted = stationsFormatted[i];
+    obj.departures = [];
+    const trLen = trainsData.length;
+    for (let j = 0; j < trLen; ++j) {
+      if (trainsData[j].itinerary[i]) {
+        const departure: StationDeparture = {
+          time: 0,
+          trainDetails: {
+            id: 7101,
+            directionId: 1,
+            activeOnWeekendsAndHolidays: false,
+          },
+        };
+        departure.time = trainsData[j].itinerary[i].time;
+        departure.trainDetails = {
+          id: trainsData[j].id,
+          directionId: trainsData[j].directionId,
+          activeOnWeekendsAndHolidays:
+            trainsData[j].activeOnWeekendsAndHolidays,
+        };
+        obj.departures.push(departure);
       }
-      arr.push(obj); 
     }
-    return arr
+    arr.push(obj);
   }
+  return arr;
+}
 
-export function writeStationsEndpoint(
-  stationsData: Station[],
-  holidays: string[]
-  ): void {
-   const data = {
+export function writeStationsEndpoint(stationsData: Station[]): void {
+  const { holidays } = require("./extractedData");
+  const data = {
     holidays: holidays,
-    stations: stationsData
-   }   
-   return fs.writeFile("../stations.json", JSON.stringify(data, null, 2), (err) => {
-    console.log(err)
-   }) 
-  }
-  
+    stations: stationsData,
+  };
+  return fs.writeFile(
+    "../stations.json",
+    JSON.stringify(data, null, 2),
+    (err) => {
+      console.log(err);
+    }
+  );
+}
--- trainScheduleTypes.d.ts
+++ trainScheduleTypes.d.ts
@@ -1,19 +1,138 @@
+type StationName =
+  | "batajnica"
+  | "kamendin"
+  | "zemunsko polje"
+  | "altina"
+  | "zemun"
+  | "tosin bunar"
+  | "novi beograd"
+  | "beograd centar"
+  | "karadjordjev park"
+  | "vukov spomenik"
+  | "pancevacki most"
+  | "krnjaca most"
+  | "krnjaca ukr"
+  | "sebes"
+  | "ovca";
+
+type StationNameFormatted =
+  | "Batajnica"
+  | "Kamendin"
+  | "Zemunsko polje"
+  | "Altina"
+  | "Zemun"
+  | "Tošin bunar"
+  | "Novi Beograd"
+  | "Beograd centar"
+  | "Karađorđev park"
+  | "Vukov spomenik"
+  | "Pančevački most"
+  | "Krnjača most"
+  | "Krnjača ukr."
+  | "Sebeš"
+  | "Ovča";
+
+type TrainIdDirection1 =
+  | 7101
+  | 7901
+  | 8001
+  | 8003
+  | 8201
+  | 8005
+  | 8007
+  | 8009
+  | 8011
+  | 8013
+  | 8015
+  | 8203
+  | 8017
+  | 8019
+  | 8021
+  | 8023
+  | 8025
+  | 8027
+  | 8029
+  | 8031
+  | 7113
+  | 8033
+  | 8035
+  | 7905
+  | 8037
+  | 8039
+  | 8041
+  | 8043
+  | 8045;
+
+type TrainIdDirection2 =
+  | 8000
+  | 7900
+  | 8002
+  | 8004
+  | 8006
+  | 8008
+  | 8010
+  | 8012
+  | 8200
+  | 8014
+  | 8310
+  | 8016
+  | 8018
+  | 8020
+  | 8022
+  | 8024
+  | 8026
+  | 7108
+  | 8028
+  | 8030
+  | 8032
+  | 7110
+  | 8034
+  | 8036
+  | 8038
+  | 8040
+  | 8042
+  | 8202
+  | 8044
+  | 7114
+  | 8046
+  | 8048
+  | 7116
+  | 8204
+  | 8050
+  | 8340;
+
 type TrainDetails = {
-  id: number;
-  directionId: 1 | 2;
+  id: TrainIdDirection1 | TrainIdDirection2;
+  directionId: GetDirectionId<
+    TrainIdDirection1,
+    TrainIdDirection2,
+    TrainDetails["id"]
+  >;
   activeOnWeekendsAndHolidays: boolean | "w&h_only";
 };
 
-type TrainItinerary = { station: string; time: number }[];
+type GetDirectionId<T1, T2, Id extends T1 | T2> = Id extends T1 ? 1 : 2;
+
+type TrainItinerary = { station: StationName; time: number }[];
 
 type Train = TrainDetails & { itinerary: TrainItinerary };
 
 type StationDeparture = { time: number; trainDetails: TrainDetails };
 
 type Station = {
-  name: string;
-  nameFormatted: string;
+  name: StationName;
+  nameFormatted: StationNameFormatted;
   departures: StationDeparture[];
 };
 
-export { TrainDetails, TrainItinerary, Train, StationDeparture, Station };
+export {
+  TrainIdDirection1,
+  TrainIdDirection2,
+  TrainDetails,
+  TrainItinerary,
+  Train,
+  StationName,
+  StationNameFormatted,
+  Station,
+  StationDeparture,
+};
